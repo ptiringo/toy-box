5dd33e4c6ded75663f9b6cdaf9e92d73e865ef90
Matsui <ptiringo@gmail.com>
2025-07-25 15:56:39 +0900

    Jockeyクラスとそのテストを追加

---
+++ api/src/main/kotlin/com/example/api/domain/horse_racing/Jockey.kt
@@ -0,0 +1,9 @@
+package com.example.api.domain.horse_racing
+
+/** 騎手 */
+class Jockey(
+    /** 名 */
+    val firstName: String,
+    /** 姓 */
+    val lastName: String,
+)
---
+++ api/src/test/kotlin/com/example/api/domain/horse_racing/JockeyTest.kt
@@ -0,0 +1,15 @@
+package com.example.api.domain.horse_racing
+
+import org.junit.jupiter.api.DisplayName
+import kotlin.test.Test
+
+class JockeyTest {
+
+    @Test
+    @DisplayName("コンストラクタ")
+    fun testJockey() {
+        val jockey = Jockey("武", "豊")
+        assert(jockey.firstName == "武")
+        assert(jockey.lastName == "豊")
+    }
+}
