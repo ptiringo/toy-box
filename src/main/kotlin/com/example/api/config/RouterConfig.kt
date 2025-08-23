cee8b898e82981028cfa296bafd239efd26712af
Matsui <ptiringo@gmail.com>
2025-08-23 16:59:59 +0900

    feat: Springdoc OpenAPIの設定を追加
    - application.ymlにspringdoc設定を追加し、Actuatorを表示するようにしました。
    - build.gradle.ktsにspringdoc-openapi-gradle-pluginを追加しました。
    - HelloHandler.ktにOpenAPIのOperationアノテーションを追加しました。
    - RouterConfig.ktにOpenAPIDefinitionとRouterOperationsアノテーションを追加しました。

--- src/main/kotlin/com/example/api/config/RouterConfig.kt
+++ src/main/kotlin/com/example/api/config/RouterConfig.kt
@@ -1,6 +1,10 @@
 package com.example.api.config
 
 import com.example.api.handler.HelloHandler
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import org.springdoc.core.annotations.RouterOperation
+import org.springdoc.core.annotations.RouterOperations
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.server.RouterFunction
@@ -8,9 +12,13 @@ import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.coRouter
 
 @Configuration
+@OpenAPIDefinition(info = Info(title = "toy-box", summary = "toy-box の API 定義です。"))
 class RouterConfig {
 
     @Bean
+    @RouterOperations(
+        RouterOperation(beanClass = HelloHandler::class, beanMethod = "hello"),
+    )
     fun helloRoute(helloHandler: HelloHandler): RouterFunction<ServerResponse> {
         return coRouter {
             GET("/api/hello", helloHandler::hello)
--- src/main/kotlin/com/example/api/handler/HelloHandler.kt
+++ src/main/kotlin/com/example/api/handler/HelloHandler.kt
@@ -1,5 +1,6 @@
 package com.example.api.handler
 
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
@@ -8,6 +9,11 @@ import org.springframework.web.reactive.function.server.bodyValueAndAwait
 @Component
 class HelloHandler {
 
+    @Operation(
+        summary = "Hello World エンドポイント",
+        description = "簡単な Hello World メッセージを返すエンドポイント",
+        tags = ["Hello"]
+    )
     suspend fun hello(@Suppress("unused") request: ServerRequest): ServerResponse {
         return ServerResponse.ok()
             .bodyValueAndAwait(mapOf("message" to "Hello World"))
